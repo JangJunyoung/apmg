838a026bd3a88ec7738416e42b097b5fa76568a4
JunyoungJang <tintwofive@gmail.com>
2015-10-23 11:25:55 +0900

    수정

--- apmg_web/apmg_app/models.py
+++ apmg_web/apmg_app/models.py
@@ -1,3 +1,15 @@
+# coding: utf-8
 from django.db import models
 
 # Create your models here.
+class Spec(models.Model):
+    name = models.CharField(max_length=100)
+
+class Charge(models.Model):
+    name = models.CharField(max_length=100)
+    at = models.DateTimeField(verbose_name=u'날짜')
+
+class UnpaidCharge(models.Model):
+    name = models.CharField(max_length=100)
+    amount = models.IntegerField(verbose_name=u'금액')
+    at = models.DateTimeField(verbose_name=u'날짜')    
